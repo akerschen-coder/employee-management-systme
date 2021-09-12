19e7dd429c90d145ee5a712edbee0e950c2f7636
Anastayzia Kerschen <anastayzia.kerschen@gmail.com>
2021-09-12 16:15:55 -0600

    made schema in db

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employeesystem_db;
+CREATE DATABASE employeesystem_db;
+
+USE employeesystem_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary INT(20),
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) 
+  REFERENCES department(id) 
+  ON DELETE CASCADE
+); 
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL 
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE CASCADE
+)
